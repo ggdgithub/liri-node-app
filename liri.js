a3925f1548691e9143f85dd75eaaa400a477a3fc
Gabriel de Leon <gabriel.deleon0126@gmail.com>
2019-08-19 15:13:02 -0700

    pseudocode of app added

--- liri.js
+++ liri.js
@@ -2,10 +2,17 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
+// var artist = something;
+// the commands
+    // node liri.js concert-this <artist/band name here>
+    // node liri.js spotify-this-song '<song name here>'
+    // node liri.js movie-this '<movie name here>'
+
 // GOAL OF APP:
     // accept input
         // song
         // film
+        // artist
     // push results using axios
         // info about song
             // artist(s)
