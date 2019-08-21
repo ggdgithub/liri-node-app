e64d8c6f7b9b86b7abfcf3a643bc04857225b8b5
Gabriel de Leon <gabriel.deleon0126@gmail.com>
2019-08-20 20:08:10 -0700

    able to return json data of a single track by ID; added variable to hold process.argv stuff

--- liri.js
+++ liri.js
@@ -1,18 +1,51 @@
+// requires
 require("dotenv").config();
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+
+// "spotify-this-song"
+spotify
+    .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+    .then(function (data) {
+        console.log(data);
+    })
+    .catch(function (err) {
+        console.error('Error occurred: ' + err);
+    });
 
 // var artist = something;
 // the commands
+    // maybe this goes here?
+    // axios({
+    //     url: 'https://accounts.spotify.com/api/token',
+    //     method: 'post',
+    //     params: {
+    //       grant_type: 'client_credentials'
+    //     },
+    //     headers: {
+    //       'Accept':'application/json',
+    //       'Content-Type': 'application/x-www-form-urlencoded'
+    //     },
+    //     auth: {
+    //       username: 'YOUR-CLIENT-ID',
+    //       password: 'YOUR-CLIENT-SECRET'
+    //     }
+    //   }).then(function(response) {
+    //       console.log(response);
+    //   }).catch(function(error) {
+    //   });
     // node liri.js concert-this <artist/band name here>
+        // somehow use this: "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
     // node liri.js spotify-this-song '<song name here>'
     // node liri.js movie-this '<movie name here>'
+    // node liri.js do-what-it-says
 
 // GOAL OF APP:
     // accept input
         // song
         // film
-        // artist
+        // artist/concert
     // push results using axios
         // info about song
             // artist(s)
